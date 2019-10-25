9a55d48adfa8d6048a60bc9b85a1b2c8beea4316
sahu0957 <sahu0957@colorado.edu>
2019-10-25 20:46:54 +0000

    functional tests work here, but not on travis?

--- test_data_import.sh
+++ test_data_import.sh
@@ -2,11 +2,6 @@ test -e ssshtest || wget -q https://raw.githubusercontent.com/ryanlayer/ssshtest
 
 . ssshtest
 
-#if the file doesn't exist, we should just exit with an error
-
-run data_import_test python data_import.py smallData test.txt foo.csv
-assert_exit_code 1
-
 # try to run with proper inputs
 run data_import_test python data_import.py smallData cgm_key cgm_small.csv
 assert_exit_code 0
